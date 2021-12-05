fce31913feebe0ecbdb9243f16566e6ad1d21603
Yasin Altunisik <altun.yasin.isik@gmail.com>
2021-12-05 09:18:29 +0300

    comitted all

---
+++ HW02_Extension/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace HW02
+{
+    public static class Extension
+    {
+        public static string ToTl(this double value)
+        {
+            return Convert.ToString(value * 13)+"TL";
+        }
+    }
+}
---
+++ HW02_Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HW02;
+
+namespace HW02_Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var amount = 1;
+
+            //Dollar equation
+            Console.WriteLine("1 Dolar : " + Extension.ToTl(amount));
+            Console.ReadLine();
+        }
+    }
+}
